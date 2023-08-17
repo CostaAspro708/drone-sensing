63f92c6f0c6f8d1a16c401409243741422e2e7a9
Costa Aspromourgos <n10464174@qut.edu.au>
2023-08-17 10:18:51 +1000

    sesnsing

--- front-end/src/components/WebSocketCall.jsx
+++ front-end/src/components/WebSocketCall.jsx
@@ -3,6 +3,13 @@ import { useEffect, useState } from "react";
 export default function WebSocketCall({ socket }) {
   const [time, setTime] = useState([]);
   const [image, setImage] = useState([]);
+  const [temperature, setTemperature] = useState([]);
+  const [pressure, setPressure] = useState([]);
+  const [humidity, setHumidity] = useState([]);
+  const [light, setLight] = useState([]);
+
+  // for each sensing reading. 
+
 
   useEffect(() => {
     socket.on("time", (data) => {
@@ -10,6 +17,36 @@ export default function WebSocketCall({ socket }) {
       setTime(data.data);
     });
   }, [socket, time]);
+
+  useEffect(() => {
+    socket.on("temperature", (data) => {
+      // setMessages([...messages, data.data]);
+      setTemperature(data.data);
+    });
+  }, [socket, temperature]);
+
+  useEffect(() => {
+    socket.on("pressure", (data) => {
+      // setMessages([...messages, data.data]);
+      setPressure(data.data);
+    });
+  }, [socket, pressure]);
+  
+  useEffect(() => {
+    socket.on("humidity", (data) => {
+      // setMessages([...messages, data.data]);
+      setHumidity(data.data);
+    });
+  }, [socket, humidity]);
+
+  useEffect(() => {
+    socket.on("light", (data) => {
+      // setMessages([...messages, data.data]);
+      setLight(data.data);
+    });
+  }, [socket, light]);
+  
+  
 
   useEffect(() => {
     socket.on("image", (data) => {
@@ -23,6 +60,10 @@ export default function WebSocketCall({ socket }) {
     <div>
       <h2>WebSocket Communication</h2>
       <h1 className="text-3xl font-bold underline"> {time} </h1>
+      <h1> temp {temperature}c</h1>
+      <h1>pressure {pressure} hPa </h1>
+      <h1>humidity {humidity} % </h1>
+      <h1>light {light} lux </h1>
       <img
           src={image}
           alt="Received Image"
--- server.py
+++ server.py
@@ -16,6 +16,176 @@ socketio = SocketIO(app,cors_allowed_origins="*")
 
 current_connections = set()
 
+#!/usr/bin/env python3
+
+import time
+import colorsys
+import os
+import sys
+import ST7735
+try:
+    # Transitional fix for breaking change in LTR559
+    from ltr559 import LTR559
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
+
+from bme280 import BME280
+from enviroplus import gas
+from subprocess import PIPE, Popen
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from fonts.ttf import RobotoMedium as UserFont
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("""all-in-one.py - Displays readings from all of Enviro plus' sensors
+Press Ctrl+C to exit!
+""")
+
+# BME280 temperature/pressure/humidity sensor
+bme280 = BME280()
+
+# Create ST7735 LCD display class
+st7735 = ST7735.ST7735(
+    port=0,
+    cs=1,
+    dc=9,
+    backlight=12,
+    rotation=270,
+    spi_speed_hz=10000000
+)
+
+# Initialize display
+st7735.begin()
+
+WIDTH = st7735.width
+HEIGHT = st7735.height
+
+# Set up canvas and font
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+draw = ImageDraw.Draw(img)
+path = os.path.dirname(os.path.realpath(__file__))
+font_size = 20
+font = ImageFont.truetype(UserFont, font_size)
+
+message = ""
+
+# The position of the top bar
+top_pos = 25
+
+def display_IP(variable):
+    # Maintain length of list
+    values[variable] = values[variable][1:] + [1]
+    # Scale the values for the variable between 0 and 1
+    vmin = min(values[variable])
+    vmax = max(values[variable])
+    colours = [(v - vmin + 1) / (vmax - vmin + 1) for v in values[variable]]
+    # Format the variable name and value
+    message = get_ip_address()
+    draw.rectangle((0, 0, WIDTH, HEIGHT), (255, 255, 255))
+    for i in range(len(colours)):
+        # Convert the values to colours from red to blue
+        colour = (1.0 - colours[i]) * 0.6
+        r, g, b = [int(x * 255.0) for x in colorsys.hsv_to_rgb(colour, 1.0, 1.0)]
+        # Draw a 1-pixel wide rectangle of colour
+        draw.rectangle((i, top_pos, i + 1, HEIGHT), (r, g, b))
+        # Draw a line graph in black
+        line_y = HEIGHT - (top_pos + (colours[i] * (HEIGHT - top_pos))) + top_pos
+        draw.rectangle((i, line_y, i + 1, line_y + 1), (0, 0, 0))
+    # Write the text at the top in black
+    draw.text((0, 0), message, font=font, fill=(0, 0, 0))
+    st7735.display(img)
+
+# Displays data and text on the 0.96" LCD
+def display_text(variable, data, unit):
+    # Maintain length of list
+    values[variable] = values[variable][1:] + [data]
+    # Scale the values for the variable between 0 and 1
+    vmin = min(values[variable])
+    vmax = max(values[variable])
+    colours = [(v - vmin + 1) / (vmax - vmin + 1) for v in values[variable]]
+    # Format the variable name and value
+    message = "{}: {:.1f} {}".format(variable[:4], data, unit)
+    logging.info(message)
+    draw.rectangle((0, 0, WIDTH, HEIGHT), (255, 255, 255))
+    for i in range(len(colours)):
+        # Convert the values to colours from red to blue
+        colour = (1.0 - colours[i]) * 0.6
+        r, g, b = [int(x * 255.0) for x in colorsys.hsv_to_rgb(colour, 1.0, 1.0)]
+        # Draw a 1-pixel wide rectangle of colour
+        draw.rectangle((i, top_pos, i + 1, HEIGHT), (r, g, b))
+        # Draw a line graph in black
+        line_y = HEIGHT - (top_pos + (colours[i] * (HEIGHT - top_pos))) + top_pos
+        draw.rectangle((i, line_y, i + 1, line_y + 1), (0, 0, 0))
+    # Write the text at the top in black
+    draw.text((0, 0), message, font=font, fill=(0, 0, 0))
+    st7735.display(img)
+
+
+# Get the temperature of the CPU for compensation
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
+    output, _error = process.communicate()
+    return float(output[output.index('=') + 1:output.rindex("'")])
+
+
+# Tuning factor for compensation. Decrease this number to adjust the
+# temperature down, and increase to adjust up
+factor = 2.25
+
+cpu_temps = [get_cpu_temperature()] * 5
+
+delay = 0.5  # Debounce the proximity tap
+mode = 0  # The starting mode
+last_page = 0
+light = 1
+
+# Create a values dict to store the data
+variables = ["temperature",
+             "pressure",
+             "humidity",
+             "light"]
+
+values = {}
+
+for v in variables:
+    values[v] = [1] * WIDTH
+    
+def sensor_loop():
+    cpu_temps = [get_cpu_temperature()] * 5
+    while True:
+        # proximity = ltr559.get_proximity()
+        # variable = "temperature"
+        unit = "C"
+        cpu_temp = get_cpu_temperature()
+        # Smooth out with some averaging to decrease jitter
+        cpu_temps = cpu_temps[1:] + [cpu_temp]
+        avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+        raw_temp = bme280.get_temperature()
+        data = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
+        socketio.emit('temperature', {'data': data}, broadcast=True)
+
+        # Preassure 
+        data = bme280.get_pressure()
+        socketio.emit('pressure', {'data': data}, broadcast=True)
+
+        #Humidity
+        data = bme280.get_humidity()
+        socketio.emit('humidity', {'data': data}, broadcast=True)
+
+        # Light
+        data = data = ltr559.get_lux() 
+        socketio.emit('light', {'data': data}, broadcast=True)
+
+
+        eventlet.sleep(1)
+
 def send_image(image):
     _, buffer = cv2.imencode('.jpg', image)
     image_data = base64.b64encode(buffer).decode('utf-8')
@@ -74,6 +244,7 @@ def connected():
     if(len(current_connections) == 0):
         eventlet.spawn(gettime)  # Start sending time in a separate thread when first client connects
         eventlet.spawn(camera_loop)  # Start sending time in a separate thread when first client connects
+        eventlet.spawn(sensor_loop)
     current_connections.add(request.sid)
     print("client has connected")
     emit("connect",{"data":f"id: {request.sid} is connected"})
@@ -93,15 +264,16 @@ def disconnected():
 
 def get_ip_address():
     try:
-        # Get the local hostname
-        hostname = socket.gethostname()
-        # Get the IP address associated with the hostname
-        ip_address = socket.gethostbyname(hostname)
-        return ip_address
+        gw = os.popen("ip -4 route show default").read().split()
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((gw[2], 0))
+        ipaddr = s.getsockname()[0]
+        return ipaddr
     except Exception as e:
         print("Error:", e)
         return None
     
 if __name__ == '__main__':
+    display_IP("temperature")
     socketio.run(app, debug=True,port=5001, host='0.0.0.0')
     # print(get_ip_address())
